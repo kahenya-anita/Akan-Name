4dcae7fe9ae04fdcc974dd72c9137d0fd4a7c463
anitakahenya1@gmail.com <santa@northpole.com>
2020-04-13 18:36:21 +0300

    Changes in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ function getAkanName(){
     var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
     var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
     var myBirthday = document.getElementById("myBirthDate").value;
-    var myGender = document.getElementsByName("gender");
+    var myGender = document.getElementsById("male", "female");
     var dateOfBirth = new Date(myBirthday);
     var dayOfTheWeek = dateOfBirth.getDay();
     if(myBirthday === ""){
